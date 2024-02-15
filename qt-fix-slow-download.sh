4c4e5b846fe84cdc3c7788ac6e57a210cefca7e1
Gabriel Pivato <pivatogabriel@gmail.com>
2024-02-15 18:02:05 -0300

    qt mirror workaround

---
+++ qt-fix-slow-download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./qt-unified-linux-x64-4.6.1-online.run --mirror https://mirrors.ocf.berkeley.edu/qt/
+
+# if already installed:
+# ./MaintenanceTool --mirror https://mirrors.ocf.berkeley.edu/qt/
