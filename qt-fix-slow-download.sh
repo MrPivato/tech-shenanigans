9afcc6c8293d37d3a0d40061fdfbb6c804a3ed89
Gabriel Pivato <pivatogabriel@gmail.com>
2026-01-30 08:17:17 -0300

    Add mirror list URL comment to download script
    Added a comment with mirror list URL for downloads.

--- qt-fix-slow-download.sh
+++ qt-fix-slow-download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Mirrors can be found here:
+# https://download.qt.io/online/qtsdkrepository/windows_x86/root/qt/Updates.xml.mirrorlist
 ./qt-unified-linux-x64-4.6.1-online.run --mirror https://mirrors.ocf.berkeley.edu/qt/
 
 # if already installed:
